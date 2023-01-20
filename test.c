1cbe837deee3b350387fbf1c4c0135144fec472b
guo <979877847@qq.com>
2023-01-21 01:30:40 +0800

    2023-1-21 1:32023-1-21 1:

--- test.c
+++ test.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char argv[]){
-	int **a = (int**)malloc(10 * sizeof(int*));
-	register int i=0;
-	for(; i < 10; i++){
-		a[i] = (int*)malloc(sizeof(int*));
-		a[i][0] = 1;
-	}i--;
-	printf("------%d %d------\n", i, a[i][0]);
-	a = realloc(a, 20 * sizeof(int*));
-	for(i = 10; i < 20; i++){
-		a[i] = (int*)malloc(sizeof(int));
-		a[i][0] = 1;
-	}i--;
-	printf("-------%d, %d-------\n", i, a[i][0]);
-	return 0;
+union ab{
+	int a;
+	char b;
+};
+
+int main(){
+	union ab one;
+	one.a=1;
+	printf("%d, %c\n", one.a, one.b);
+	printf("%p, %p\n",one.a, one.b);
+	one.b = '2';
+	printf("%d, %c\n", one.a, one.b);
+	printf("%p, %p\n",one.a, one.b);
 }
